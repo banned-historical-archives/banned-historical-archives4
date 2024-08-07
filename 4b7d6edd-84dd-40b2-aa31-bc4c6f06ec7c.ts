dfc2a38fc2139a7d970755d8665a315f59215e60
PirateBook1 <news_porter@protonmail.com>
2024-08-08 03:26:40 +0800

    1

---
+++ 4b7d6edd-84dd-40b2-aa31-bc4c6f06ec7c.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "4b7d6edd-84dd-40b2-aa31-bc4c6f06ec7c",
+    "name": "我亲历的国防科委文革（中国文化传播出版社2012年）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "恽仁祥",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/4b7d6edd-84dd-40b2-aa31-bc4c6f06ec7c.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "我亲历的国防科委文革",
+        "authors": [
+          "恽仁祥"
+        ],
+        "page_start": 6,
+        "page_end": 515,
+        "dates": [
+          {
+            "year": 2012,
+            "month": 7
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "4b7d6edd-84dd-40b2-aa31-bc4c6f06ec7c.pdf",
+  "resource_type": "book",
+  "version": 2
+}
