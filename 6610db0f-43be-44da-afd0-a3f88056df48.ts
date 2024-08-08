76cad683d53a265777c64841f222b0e593cfccee
PirateBook1 <news_porter@protonmail.com>
2024-08-08 11:33:56 +0800

    1

---
+++ 6610db0f-43be-44da-afd0-a3f88056df48.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "6610db0f-43be-44da-afd0-a3f88056df48",
+    "name": "非常年代——1964-1978（下）（牛津大学出版社2019年）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "赵园，牛津大学出版社2019年",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/6610db0f-43be-44da-afd0-a3f88056df48.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "非常年代——1964-1978（下）",
+        "authors": [
+          "赵园"
+        ],
+        "page_start": 8,
+        "page_end": 680,
+        "dates": [
+          {
+            "year": 2019
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "6610db0f-43be-44da-afd0-a3f88056df48.pdf",
+  "resource_type": "book",
+  "version": 2
+}
