ef4ae367fb2201440ae714582e83b93f6e3432c7
PirateBook1 <news_porter@protonmail.com>
2024-08-08 21:43:49 +0800

    1

---
+++ ba90468d-3a39-40e5-bd9f-828537826779.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "ba90468d-3a39-40e5-bd9f-828537826779",
+    "name": "文革大屠杀（开放杂志社2002年）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "宋永毅，开放杂志社2002年",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/ba90468d-3a39-40e5-bd9f-828537826779.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "文革大屠杀",
+        "authors": [
+          "宋永毅"
+        ],
+        "page_start": 2,
+        "page_end": 152,
+        "dates": [
+          {
+            "year": 2002,
+            "month": 7
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "ba90468d-3a39-40e5-bd9f-828537826779.pdf",
+  "resource_type": "book",
+  "version": 2
+}
