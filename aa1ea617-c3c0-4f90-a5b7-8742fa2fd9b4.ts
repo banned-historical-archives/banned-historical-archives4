8fffb5fdaa91d4d355bccb68913816ffb085a0d9
PirateBook1 <news_porter@protonmail.com>
2024-08-08 21:38:42 +0800

    1

---
+++ aa1ea617-c3c0-4f90-a5b7-8742fa2fd9b4.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "aa1ea617-c3c0-4f90-a5b7-8742fa2fd9b4",
+    "name": "文革时期中国农村的集体杀戮（中文大学出版社2017年）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "苏阳，中文大学出版社2017年",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/aa1ea617-c3c0-4f90-a5b7-8742fa2fd9b4.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "文革时期中国农村的集体杀戮",
+        "authors": [
+          "苏阳"
+        ],
+        "page_start": 6,
+        "page_end": 325,
+        "dates": [
+          {
+            "year": 2017
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "aa1ea617-c3c0-4f90-a5b7-8742fa2fd9b4.pdf",
+  "resource_type": "book",
+  "version": 2
+}
