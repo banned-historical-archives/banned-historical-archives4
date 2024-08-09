0e3368ef089902a62e993d1835e39630b8d2028b
PirateBook1 <news_porter@protonmail.com>
2024-08-09 18:04:30 +0800

    1

---
+++ 0413497b-e274-4241-aaad-a9c844d82d47.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "0413497b-e274-4241-aaad-a9c844d82d47",
+    "name": "Fractured Rebellion - The Beijing Red Guard Movement",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "魏昂德，哈佛大学出版社2009年",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/0413497b-e274-4241-aaad-a9c844d82d47.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "Fractured Rebellion - The Beijing Red Guard Movement",
+        "authors": [
+          "魏昂德"
+        ],
+        "page_start": 12,
+        "page_end": 417,
+        "dates": [
+          {
+            "year": 2023
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "0413497b-e274-4241-aaad-a9c844d82d47.pdf",
+  "resource_type": "book",
+  "version": 2
+}
