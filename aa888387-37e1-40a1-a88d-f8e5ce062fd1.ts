a46de732342419a2fa9aab6e1a82bc0d82a05b7a
PirateBook1 <news_porter@protonmail.com>
2024-08-09 18:01:39 +0800

    1

---
+++ aa888387-37e1-40a1-a88d-f8e5ce062fd1.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "aa888387-37e1-40a1-a88d-f8e5ce062fd1",
+    "name": "Agents of Disorder - Inside China’s Cultural Revolution",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "魏昂德，哈佛大学出版社2019年",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/aa888387-37e1-40a1-a88d-f8e5ce062fd1.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "Agents of Disorder - Inside China’s Cultural Revolution",
+        "authors": [
+          "魏昂德"
+        ],
+        "page_start": 10,
+        "page_end": 286,
+        "dates": [
+          {
+            "year": 2019
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "aa888387-37e1-40a1-a88d-f8e5ce062fd1.pdf",
+  "resource_type": "book",
+  "version": 2
+}
