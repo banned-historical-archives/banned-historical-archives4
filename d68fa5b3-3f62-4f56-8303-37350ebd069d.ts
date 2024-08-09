f90e097df35fac48001c820b7ec8ed777fb361e2
PirateBook1 <news_porter@protonmail.com>
2024-08-09 18:03:08 +0800

    1

---
+++ d68fa5b3-3f62-4f56-8303-37350ebd069d.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "d68fa5b3-3f62-4f56-8303-37350ebd069d",
+    "name": "Civil War in Guangxi - The Cultural Revolution on China's Southern Periphery",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "魏昂德，斯坦福大学出版社2023年",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/d68fa5b3-3f62-4f56-8303-37350ebd069d.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "Civil War in Guangxi - The Cultural Revolution on China's Southern Periphery",
+        "authors": [
+          "魏昂德"
+        ],
+        "page_start": 14,
+        "page_end": 297,
+        "dates": [
+          {
+            "year": 2023
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "d68fa5b3-3f62-4f56-8303-37350ebd069d.pdf",
+  "resource_type": "book",
+  "version": 2
+}
