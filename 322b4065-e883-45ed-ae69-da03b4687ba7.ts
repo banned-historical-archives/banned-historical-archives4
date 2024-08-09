96e72f475f78cad41857436231a3e3c6f1489c7e
PirateBook1 <news_porter@protonmail.com>
2024-08-09 17:59:40 +0800

    1

---
+++ 322b4065-e883-45ed-ae69-da03b4687ba7.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "322b4065-e883-45ed-ae69-da03b4687ba7",
+    "name": "A Social History of Maoist China Conflict and Change",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "文浩，剑桥大学出版社2019年",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/322b4065-e883-45ed-ae69-da03b4687ba7.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "A Social History of Maoist China Conflict and Change",
+        "authors": [
+          "文浩"
+        ],
+        "page_start": 14,
+        "page_end": 379,
+        "dates": [
+          {
+            "year": 2019
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "322b4065-e883-45ed-ae69-da03b4687ba7.pdf",
+  "resource_type": "book",
+  "version": 2
+}
