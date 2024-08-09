225373c800445c40ac1e264317f16647cf5b45c8
PirateBook1 <news_porter@protonmail.com>
2024-08-09 17:56:31 +0800

    1

---
+++ 54234aaf-f720-42e8-95bf-141323d8b1f8.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "54234aaf-f720-42e8-95bf-141323d8b1f8",
+    "name": "A Decade of Upheaval The Cultural Revolution in Rural China",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "魏昂德，普林斯顿大学出版社2021年",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/54234aaf-f720-42e8-95bf-141323d8b1f8.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "A Decade of Upheaval The Cultural Revolution in Rural China",
+        "authors": [
+          "魏昂德"
+        ],
+        "page_start": 7,
+        "page_end": 239,
+        "dates": [
+          {
+            "year": 2021
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "54234aaf-f720-42e8-95bf-141323d8b1f8.pdf",
+  "resource_type": "book",
+  "version": 2
+}
