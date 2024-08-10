841f52b5cefafda75623ab6819ba0cd1ea228481
PirateBook1 <news_porter@protonmail.com>
2024-08-10 18:31:07 +0800

    1

---
+++ c6214161-e577-46df-8de2-967ef1ce6d3e.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "c6214161-e577-46df-8de2-967ef1ce6d3e",
+    "name": "The Chinese Cultural Revolution - a history",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "Paul Clark，剑桥大学出版社2008年",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/c6214161-e577-46df-8de2-967ef1ce6d3e.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "The Chinese Cultural Revolution - a history",
+        "authors": [
+          "Paul",
+          "Clark"
+        ],
+        "page_start": 9,
+        "page_end": 368,
+        "dates": [
+          {
+            "year": 2008
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "c6214161-e577-46df-8de2-967ef1ce6d3e.pdf",
+  "resource_type": "book",
+  "version": 2
+}
