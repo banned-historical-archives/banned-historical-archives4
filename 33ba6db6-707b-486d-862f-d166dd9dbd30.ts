71884dd68a5bb905fbdb7432336fcd1cfcab68df
PirateBook1 <news_porter@protonmail.com>
2024-08-10 20:02:24 +0800

    1

---
+++ 33ba6db6-707b-486d-862f-d166dd9dbd30.ts
@@ -0,0 +1,33 @@
+export default {
+  "entity": {
+    "id": "33ba6db6-707b-486d-862f-d166dd9dbd30",
+    "name": "麻与黑（中国文史出版社2007年）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "骆漠，中国文史出版社2007年5月",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/33ba6db6-707b-486d-862f-d166dd9dbd30.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "麻与黑",
+        "authors": [
+          "骆漠"
+        ],
+        "page_start": 3,
+        "page_end": 222,
+        "dates": [
+          {
+            "year": 2007,
+            "month": 5
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "33ba6db6-707b-486d-862f-d166dd9dbd30.pdf",
+  "resource_type": "book",
+  "version": 2
+}
