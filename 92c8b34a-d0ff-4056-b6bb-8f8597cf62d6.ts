6e161dc2c7366602c8a8a162ae69c7c429238a40
PirateBook1 <news_porter@protonmail.com>
2024-08-10 18:34:59 +0800

    1

---
+++ 92c8b34a-d0ff-4056-b6bb-8f8597cf62d6.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "92c8b34a-d0ff-4056-b6bb-8f8597cf62d6",
+    "name": "The Paris Commune in Shanghai the Masses, the State, and Dynamics of \"Continuous Revolution\"",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "蒋洪生",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/92c8b34a-d0ff-4056-b6bb-8f8597cf62d6.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "The Paris Commune in Shanghai the Masses, the State, and Dynamics of \"Continuous Revolution\"",
+        "authors": [
+          "蒋洪生"
+        ],
+        "page_start": 4,
+        "page_end": 637,
+        "dates": [
+          {
+            "year": 2010
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "92c8b34a-d0ff-4056-b6bb-8f8597cf62d6.pdf",
+  "resource_type": "book",
+  "version": 2
+}
---
+++ d4ddeb31-d4f7-4337-800a-9427f942d8f2.ts
@@ -0,0 +1,32 @@
+export default {
+  "entity": {
+    "id": "d4ddeb31-d4f7-4337-800a-9427f942d8f2",
+    "name": "The Paris Commune in Shanghai the Masses, the State, and Dynamics of “Continuous Revolution”",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "蒋洪生",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/d4ddeb31-d4f7-4337-800a-9427f942d8f2.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "The Paris Commune in Shanghai the Masses, the State, and Dynamics of “Continuous Revolution”",
+        "authors": [
+          "蒋洪生"
+        ],
+        "page_start": 4,
+        "page_end": 637,
+        "dates": [
+          {
+            "year": 2010
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "d4ddeb31-d4f7-4337-800a-9427f942d8f2.pdf",
+  "resource_type": "book",
+  "version": 2
+}
