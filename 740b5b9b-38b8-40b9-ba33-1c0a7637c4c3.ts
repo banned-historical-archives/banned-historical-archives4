9cf1a868925c3c61e1c7a03f083439844c744d3d
PirateBook1 <news_porter@protonmail.com>
2024-08-10 22:20:25 +0800

    1

---
+++ 740b5b9b-38b8-40b9-ba33-1c0a7637c4c3.ts
@@ -0,0 +1,168 @@
+export default {
+  "entity": {
+    "id": "740b5b9b-38b8-40b9-ba33-1c0a7637c4c3",
+    "name": "夺取揭批“四人帮”斗争的全胜",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "中共上海市委揭批“四人帮”运动办公室印，1978.11",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/6.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/7.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/8.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/9.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/10.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/11.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/12.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/13.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/14.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/15.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/16.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/17.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/18.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/19.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/20.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/21.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/22.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/23.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/24.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/740b5b9b-38b8-40b9-ba33-1c0a7637c4c3/25.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "夺取揭批“四人帮”斗争的全胜",
+        "authors": [
+          "中共上海市委"
+        ],
+        "page_start": 3,
+        "page_end": 4,
+        "dates": [
+          {
+            "year": 1978,
+            "month": 10
+          }
+        ]
+      },
+      {
+        "title": "继续抓紧抓好清查工作的若干问题",
+        "authors": [
+          "《运动情况》增刊"
+        ],
+        "page_start": 5,
+        "page_end": 6,
+        "dates": [
+          {
+            "year": 1978
+          }
+        ]
+      },
+      {
+        "title": "关于审查对象定性处理的审批手续等问题的意见",
+        "authors": [
+          "6",
+          "7"
+        ],
+        "page_start": 6,
+        "page_end": 7,
+        "dates": [
+          {
+            "year": 1978
+          }
+        ]
+      },
+      {
+        "title": "市委召开干部会议学习和贯彻党中央四十八号文件",
+        "authors": [
+          "《运动情况》增刊"
+        ],
+        "page_start": 7,
+        "page_end": 9,
+        "dates": [
+          {
+            "year": 1978
+          }
+        ]
+      },
+      {
+        "title": "关于对与“四人帮”有牵连的干部采取临时组织措施的若干规定",
+        "authors": [
+          "无"
+        ],
+        "page_start": 9,
+        "page_end": 10,
+        "dates": [
+          {
+            "year": 1978
+          }
+        ]
+      },
+      {
+        "title": "关于上海市清查工作的情况报告（节录）",
+        "authors": [
+          "无"
+        ],
+        "page_start": 11,
+        "page_end": 13,
+        "dates": [
+          {
+            "year": 1978
+          }
+        ]
+      },
+      {
+        "title": "严佑民同志在区县局干部会议上讲话",
+        "authors": [
+          "严佑民"
+        ],
+        "page_start": 14,
+        "page_end": 19,
+        "dates": [
+          {
+            "year": 1978,
+            "month": 4,
+            "day": 16
+          }
+        ]
+      },
+      {
+        "title": "关于《清查工作中应注意掌握的几个问题向市委的请示报告》",
+        "authors": [
+          "中共上海市委揭批“四人帮”运动办公室"
+        ],
+        "page_start": 19,
+        "page_end": 19,
+        "dates": [
+          {
+            "year": 1977,
+            "month": 12,
+            "day": 11
+          }
+        ]
+      },
+      {
+        "title": "清查工作中应注意掌握的几个问题",
+        "authors": [
+          "无"
+        ],
+        "page_start": 20,
+        "page_end": 24,
+        "dates": [
+          {
+            "year": 1978
+          }
+        ]
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "740b5b9b-38b8-40b9-ba33-1c0a7637c4c3",
+  "resource_type": "book",
+  "version": 2
+}
