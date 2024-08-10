2037783796810599245055586289a1908ff2c0a2
PirateBook1 <news_porter@protonmail.com>
2024-08-10 19:54:01 +0800

    1

---
+++ aab6954d-03a5-47da-9a5c-195b7332db41.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "aab6954d-03a5-47da-9a5c-195b7332db41",
+    "name": "牛鬼蛇神录",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "杨小凯",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/aab6954d-03a5-47da-9a5c-195b7332db41.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "牛鬼蛇神录",
+        "authors": [
+          "杨小凯"
+        ],
+        "page_start": 1,
+        "page_end": 228,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "aab6954d-03a5-47da-9a5c-195b7332db41.pdf",
+  "resource_type": "book",
+  "version": 2
+}
