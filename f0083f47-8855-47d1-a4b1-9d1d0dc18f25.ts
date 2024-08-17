64f1be0317513677ce1501aa50c284f497f47c8d
PirateBook1 <news_porter@protonmail.com>
2024-08-17 23:37:10 +0800

    1

---
+++ f0083f47-8855-47d1-a4b1-9d1d0dc18f25.ts
@@ -0,0 +1,40 @@
+export default {
+  "entity": {
+    "id": "f0083f47-8855-47d1-a4b1-9d1d0dc18f25",
+    "name": "权力结构、政治激励和经济增长——基于浙江民营经济发展经验的政治经济学分析",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "章奇 刘明兴",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives4/main/f0083f47-8855-47d1-a4b1-9d1d0dc18f25.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "权力结构、政治激励和经济增长——基于浙江民营经济发展经验的政治经济学分析",
+        "authors": [
+          "章奇",
+          "刘明兴"
+        ],
+        "page_start": 5,
+        "page_end": 279,
+        "dates": [
+          {
+            "year": 2016,
+            "month": 3
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "extract_text_from_pdf": false,
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "f0083f47-8855-47d1-a4b1-9d1d0dc18f25.pdf",
+  "resource_type": "book",
+  "version": 2
+}
